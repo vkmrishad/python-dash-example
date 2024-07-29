e1a40e385bc89487cf714513a1dc97867b1d4795
Mohammed Rishad <mohammedrishad.vk@gmail.com>
2024-07-29 23:04:55 +0200

    Updated add row logic

---
+++ src/db/schema.py
@@ -0,0 +1,27 @@
+from datetime import date
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class OrderData(BaseModel):
+    id: int
+    order_id: Optional[str]
+    order_date: Optional[date]
+    dispatch_date: Optional[date]
+    delivery_mode: Optional[str]
+    customer_id: Optional[str]
+    customer_name: Optional[str]
+    segment: Optional[str]
+    city: Optional[str]
+    state_province: Optional[str]
+    country_region: Optional[str]
+    region: Optional[str]
+    product_id: Optional[str]
+    category: Optional[str]
+    sub_category: Optional[str]
+    product_name: Optional[str]
+    sales: Optional[float]
+    quantity: Optional[int]
+    discount: Optional[float]
+    profit: Optional[float]
